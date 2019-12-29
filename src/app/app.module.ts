c3218ea2782609dede9e5c9f9f577ac87af079e9
Kamzs <kamilambroziak@interia.pl>
2019-12-29 15:22:26 +0100

    bindowanie pomiedzy child i parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChildCopmComponent } from './child-copm/child-copm.component';
 import { ChildOutputingDataComponent } from './child-outputing-data/child-outputing-data.component';
+import { NgcontentComponent } from './ngcontent/ngcontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildCopmComponent,
-    ChildOutputingDataComponent
+    ChildOutputingDataComponent,
+    NgcontentComponent
   ],
   imports: [
     BrowserModule
